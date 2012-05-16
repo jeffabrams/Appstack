d9bc47cb100115fd1f5d498d02c1f450ebc500bd
Jeff Abrams <jabrams@gobackyard.com>
2012-05-16 12:57:27 -0400

    rails

---
+++ Test2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2::Application.config.secret_token = '9edc8ef54eb027db714b08f551628b878ef9aec28fd97e7818d060ab004af0dc0e016b60707b07e01ccfc72f92c859457fc4c8494515705997ecb00a99894d80'
---
+++ testprogram/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testprogram::Application.config.secret_token = '5535677a6342474d43dcaa1d32a93a7b0421c1e0af6b960e1e4b3a4a4dae2c39d2e285ff487ad7bfcfe39b793e0d7c10071e3cd54a7674e2c843d8e0d616cc42'
